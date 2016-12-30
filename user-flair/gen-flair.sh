79cd8e8fec821d1bd053dacc7b9ba6f170397208
Hilal Alsibai <xiphirx@gmail.com>
2016-12-29 21:27:21 -0800

    No tabs

--- user-flair/gen-flair.sh
+++ user-flair/gen-flair.sh
@@ -2,7 +2,7 @@
 echo Updating user flairs...
 
 for i in $( ls *.png ); do
-	for j in $( ls *.jpg ); do
-		composite -gravity East $j $i ${i:0:(-4)}-${j:0:(-4)}.png
-	done
+  for j in $( ls *.jpg ); do
+    composite -gravity East $j $i ${i:0:(-4)}-${j:0:(-4)}.png
+  done
 done
